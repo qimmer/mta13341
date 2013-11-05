af9f7ce491d21beedd1bb81d0b751be8338efe6d
Kim Johannsen <twicfall@gmail.com>
2013-11-05 10:34:40 +0100

    Added depth image and loading procedure in test.

--- TrackerTest/mainwindow.cpp
+++ TrackerTest/mainwindow.cpp
@@ -6,6 +6,16 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QImage imgOne;
+    imgOne.load("depth.png");
+
+    QImage imgTwo;
+
+
+
+    ui->imgOne->setPixmap(QPixmap::fromImage(imgOne));
+    ui->imgTwo->setPixmap(QPixmap::fromImage(imgTwo));
 }
 
 MainWindow::~MainWindow()
