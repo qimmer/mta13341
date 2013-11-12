f2a776c447ebc1a49c27d1c494cc66871276ec8a
Nicolai Krogh Jørgensen <miberen@hotmail.com>
2013-11-12 13:23:50 +0100

    Basic shooting added

---
+++ Unity_Project/Assets/Bullet_Destroy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet_Destroy : MonoBehaviour {
+
+    public int lifespan = 5;
+
+	// Update is called once per frame
+	void Update () 
+    {
+        Destroy(gameObject, lifespan);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
