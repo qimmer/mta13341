198d1fd035220e8bdba5800961808665c52376ec
jfjclarke <jfjclarke@gmail.com>
2013-11-12 12:17:35 +0100

    Very basic Keyboard movement

---
+++ Unity_Project/Assets/Player_Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Movement : MonoBehaviour {
+
+    public float movementSpeed = 3.0F;
+ 
+    void Update () {
+
+        CharacterController controller = GetComponent<CharacterController>();
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 left = transform.TransformDirection(Vector3.left);
+
+        float currSpeed = movementSpeed * Input.GetAxis("Vertical");
+        float currSpeed2 = movementSpeed * Input.GetAxis("Horizontal");
+        controller.SimpleMove(forward * currSpeed);
+        controller.SimpleMove((-1 * left) * currSpeed2);
+
+        
+
+       
+	}
+}
