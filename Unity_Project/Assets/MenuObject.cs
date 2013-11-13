f09a2f534920c9af73238bdd4677dc8ebd0a6896
jfjclarke <jfjclarke@gmail.com>
2013-11-13 13:11:51 +0100

    Added Main menu

---
+++ Unity_Project/Assets/MenuObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuObject : MonoBehaviour {
+    
+    bool mouseOn = false;
+
+    void Update()
+    {
+        if (mouseOn == true)
+        {
+
+            float scaleX = Mathf.Cos(Time.time) * 0.1F + 1;
+            float scaleY = Mathf.Sin(Time.time) * 0.1F + 1;
+            renderer.material.SetTextureScale("_MainTex", new Vector2(scaleX, scaleY));
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        mouseOn = true;
+    }
+
+    void OnMouseExit()
+    {
+        renderer.material.SetTextureScale("_MainTex", new Vector2(1F, 1F));
+        mouseOn = false;
+    }
+
+    void OnMouseDown()
+    {
+        Application.LoadLevel(1);
+    }
+}
