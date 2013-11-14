d9c2b6befbc3d60c1a89b5056c01e8f0a7d02eea
Kim Johannsen <twicfall@gmail.com>
2013-11-14 12:44:40 +0100

    Menu Fix

--- Unity_Project/Assets/Player_Movement.cs
+++ Unity_Project/Assets/Player_Movement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Player_Movement : MonoBehaviour {
 
+    public TrackingManager Tracker = null;
     public float movementSpeed = 3.0F;
     bool isMainMenu = false;
 
