651d34ad1c6c8237ae27ef6d59a7f1bb5f30a5ba
jfjclarke <jfjclarke@gmail.com>
2013-11-21 13:59:12 +0100

    Added bunch of features to Main menu

--- Unity_Project/Assets/Scripts/MenuObject.cs
+++ Unity_Project/Assets/Scripts/MenuObject.cs
@@ -2,28 +2,56 @@
 using System.Collections;
 
 public class MenuObject : MonoBehaviour {
-    
-    bool mouseOn = false;
+
+    public float rotAngle = 0.0F;
+    private bool mouseOn = false;
+    private float posZ = 0.0F;
+    private Vector3 originalPos = new Vector3();
+    private Vector3 originalScale = new Vector3();
+    private Quaternion originalRotation = new Quaternion();
+
+
+    void Start()
+    {
+        originalPos = transform.localPosition;
+        originalScale = transform.localScale;
+        originalRotation = transform.localRotation;
+    }
 
     void Update()
     {
         if (mouseOn == true)
-        {
+        {   
+            if (this.posZ < 0.11F)
+            {
+                posZ = posZ + 0.002F;
+                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z - posZ);
+            }
 
-            float scaleX = Mathf.Cos(Time.time) * 0.1F + 1.7F;
-            float scaleY = Mathf.Sin(Time.time) * 0.1F + 1.7F;
-            float rotateX = Mathf.Cos(Time.time);
-            float rotateY = Mathf.Sin(Time.time);
-            transform.localScale = new Vector3(scaleX, scaleY, 0.3895768F);
-            //transform.localRotation = Quaternion.Euler(rotateX, rotateY, 0);
-            transform.localRotation = Quaternion.Euler(new Vector3(rotateX, rotateY, 0.3895768F));
+            float scaleX = originalScale.x + (Mathf.Cos(Time.time) * 0.1F + 5.7F);
+            float scaleY = originalScale.y + (Mathf.Sin(Time.time) * 0.1F + 5.7F);
+            float rotateX = originalRotation.x + (Mathf.Cos(Time.time) * rotAngle - 2);
+            float rotateY = originalRotation.y + (Mathf.Sin(Time.time) * rotAngle);
+            float rotateZ = originalRotation.z + (Mathf.Cos(Time.time) * rotAngle);
+            transform.localScale = new Vector3(scaleX, scaleY, transform.localScale.z);
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
+            transform.localRotation = Quaternion.Euler(new Vector3(rotateX, rotateY, transform.localRotation.z));
         }
     }
 
-    //void OnCollisionEnter(Collision collision)
-    //{
-    //    Application.LoadLevel(1);
-    //}
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(gameObject.name);
+        
+        if (gameObject.name == "CityEast")
+        {
+            Application.LoadLevel(2);
+        }
+        if (gameObject.name == "CityWest")
+        {
+            Application.LoadLevel(1);
+        }
+    }
 
     
     void OnMouseEnter()
@@ -33,14 +61,10 @@ public class MenuObject : MonoBehaviour {
 
     void OnMouseExit()
     {
-        transform.localScale = new Vector3(1.7F, 1.7F, 0.3895768F);
-        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        transform.localPosition = originalPos;
+        transform.localScale = originalScale;
+        transform.localRotation = originalRotation;
         mouseOn = false;
-    }
-
-    void OnMouseDown()
-    {
-        Application.LoadLevel(1);
-    }
-     
+        posZ = 0.0F;
+    }    
 }
