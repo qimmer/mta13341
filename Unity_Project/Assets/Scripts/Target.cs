91f7c36d15cdc474943facd5151d8fa5ce70fa8f
Nicolai Krogh Jørgensen <miberen@hotmail.com>
2013-11-21 09:39:01 +0100

    Added targets

---
+++ Unity_Project/Assets/Scripts/Target.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+    public int pointsWorth = 500;
+    public bool moving = false;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (moving)
+        {
+            //dostuff
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameScore.currentScore += pointsWorth;
+        Destroy(gameObject);  
+    }
+}
