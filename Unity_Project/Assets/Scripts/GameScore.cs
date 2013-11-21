122851c200fd41974b6a1fda5b409cb400797f3f
jfjclarke <jfjclarke@gmail.com>
2013-11-21 14:58:32 +0100

    V.C being a bitch -- fix II 	Unity_Project/Library/CurrentLayout.dwlt 	Unity_Project/Library/ScriptAssemblies/Assembly-CSharp.dll 	Unity_Project/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb 	Unity_Project/Library/assetDatabase3 	Unity_Project/Library/guidmapper 	Unity_Project/Library/metadata/eb/eb13732eda0a66b449c4e8c02b15b87a 	Unity_Project/Unity_Project-csharp.sln 	Unity_Project/Unity_Project.sln

---
+++ Unity_Project/Assets/Scripts/GameScore.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScore : MonoBehaviour {
+
+    public static int currentScore = 0;
+    public int score;
+
+	// Use this for initialization
+	void Start () 
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        score = currentScore;
+	}
+}
