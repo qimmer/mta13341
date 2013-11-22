04e7bb6b91237f431b96448418eca5121596c36d
jfjclarke <jfjclarke@gmail.com>
2013-11-22 12:38:49 +0100

    Fixed :Fix: Raycast on player and Collision detection when snowball is thrown on objects and levels are opened

--- Unity_Project/Assets/Scripts/MenuObject.cs
+++ Unity_Project/Assets/Scripts/MenuObject.cs
@@ -4,16 +4,17 @@ using System.Collections;
 public class MenuObject : MonoBehaviour {
 
     public float rotAngle = 4.0F;
-    private bool mouseOn = false;
+    private bool rayOn = false;
     private float posZ = 0.0F;
+
+    //Initial position, rotation and scale
     private Vector3 originalPos = new Vector3();
     private Vector3 originalScale = new Vector3();
     private Quaternion originalRotation = new Quaternion();
 
-    public void initScript()
+    public void rayHit(bool ray)
     {
-        Debug.Log("initScript()");
-        Start();
+        rayOn = ray;
     }
 
 
@@ -22,27 +23,33 @@ public class MenuObject : MonoBehaviour {
         originalPos = transform.localPosition;
         originalScale = transform.localScale;
         originalRotation = transform.localRotation;
-        Debug.Log("start()");
     }
 
     void Update()
     {
-        if (mouseOn == true)
-        {   
-            if (this.posZ < 0.11F)
-            {
-                posZ = posZ + 0.002F;
-                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z - posZ);
-            }
+        if (rayOn == true)
+        {
+            //if (this.posZ < 0.11F)
+            //{
+            //    posZ = posZ + 0.002F;
+            //    transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z - posZ);
+            //}
 
-            float scaleX = originalScale.x + (Mathf.Cos(Time.time) * 0.1F + 5.7F);
-            float scaleY = originalScale.y + (Mathf.Sin(Time.time) * 0.1F + 5.7F);
-            float rotateX = originalRotation.x + (Mathf.Cos(Time.time) * rotAngle - 2);
-            float rotateY = originalRotation.y + (Mathf.Sin(Time.time) * rotAngle);
-            float rotateZ = originalRotation.z + (Mathf.Cos(Time.time) * rotAngle);
-            transform.localScale = new Vector3(scaleX, scaleY, transform.localScale.z);
-            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
-            transform.localRotation = Quaternion.Euler(new Vector3(rotateX, rotateY, transform.localRotation.z));
+            //float scaleX = originalScale.x + (Mathf.Cos(Time.time) * 0.1F + 5.7F);
+            //float scaleY = originalScale.y + (Mathf.Sin(Time.time) * 0.1F + 5.7F);
+            //float rotateX = originalRotation.x + (Mathf.Cos(Time.time) * rotAngle - 2);
+            //float rotateY = originalRotation.y + (Mathf.Sin(Time.time) * rotAngle);
+            //float rotateZ = originalRotation.z + (Mathf.Cos(Time.time) * rotAngle);
+            //transform.localScale = new Vector3(scaleX, scaleY, transform.localScale.z);
+            //transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
+            //transform.localRotation = Quaternion.Euler(new Vector3(rotateX, rotateY, transform.localRotation.z));
+        }
+        else
+        {
+            transform.localPosition = originalPos;
+            transform.localScale = originalScale;
+            transform.localRotation = originalRotation;
+            //posZ = 0.0F;
         }
     }
 
@@ -59,19 +66,5 @@ public class MenuObject : MonoBehaviour {
             Application.LoadLevel(1);
         }
     }
-
-    
-    void OnMouseEnter()
-    {
-        mouseOn = true;
-    }
-
-    void OnMouseExit()
-    {
-        transform.localPosition = originalPos;
-        transform.localScale = originalScale;
-        transform.localRotation = originalRotation;
-        mouseOn = false;
-        posZ = 0.0F;
-    }    
+  
 }
--- Unity_Project/Assets/Scripts/rayCast.cs
+++ Unity_Project/Assets/Scripts/rayCast.cs
@@ -3,21 +3,27 @@ using System.Collections;
 
 public class rayCast : MonoBehaviour {
 
+    MenuObject mo;
 
 	void Update () {
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         RaycastHit myhit = new RaycastHit();
 
         //Draws the ray in the editor
-        Debug.DrawRay(transform.position, forward * 10, Color.green);
+        Debug.DrawRay(transform.position, forward * 5, Color.green);
 
         if (Physics.Raycast(transform.position, forward, out myhit, 10))
         {
-
             if (myhit.collider.name == "CityWest" || myhit.collider.name == "CityEast")
             {
-                MenuObject mo = gameObject.GetComponent<MenuObject>();
+                //mo.rayHit(true);
+                mo = myhit.collider.gameObject.GetComponent<MenuObject>();
+                mo.rayHit(true);
             }
+        }
+        else
+        {
+            mo.rayHit(false);
         }
 	}
 }
