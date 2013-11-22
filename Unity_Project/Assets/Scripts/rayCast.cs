b20bee3e18d0b0a46353880b54737bb844b6300e
jfjclarke <jfjclarke@gmail.com>
2013-11-22 09:59:06 +0100

    Fix: Raycast on player and Collision detection when snowball is thrown on objects and levels are opened

--- Unity_Project/Assets/Scripts/rayCast.cs
+++ Unity_Project/Assets/Scripts/rayCast.cs
@@ -3,14 +3,21 @@ using System.Collections;
 
 public class rayCast : MonoBehaviour {
 
-	// Use this for initialization
 
 	void Update () {
         Vector3 forward = transform.TransformDirection(Vector3.forward);
+        RaycastHit myhit = new RaycastHit();
 
-        if (Physics.Raycast(transform.position, forward, 10))
+        //Draws the ray in the editor
+        Debug.DrawRay(transform.position, forward * 10, Color.green);
+
+        if (Physics.Raycast(transform.position, forward, out myhit, 10))
         {
-            Debug.Log("hit");
+
+            if (myhit.collider.name == "CityWest" || myhit.collider.name == "CityEast")
+            {
+                MenuObject mo = gameObject.GetComponent<MenuObject>();
+            }
         }
 	}
 }
