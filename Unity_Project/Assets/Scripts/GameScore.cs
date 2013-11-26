0d90ad4bee66ad0d108504c692fa814bc54daad7
jfjclarke <jfjclarke@gmail.com>
2013-11-26 16:32:10 +0100

    Added - Spawn enemies every X sec and maxEnemy spawn increases with time

--- Unity_Project/Assets/Scripts/GameScore.cs
+++ Unity_Project/Assets/Scripts/GameScore.cs
@@ -9,12 +9,17 @@ public class GameScore : MonoBehaviour {
     public float zMax = 15.6F;
     public float zMin = 5.6F;
 
+    public int spawnTime = 15;
+    int currSpawnTime;
+
     private Quaternion originalRotation = new Quaternion();
     private Vector3 originalLocation = new Vector3();
 
     public static int currentScore = 0;
     public static int numberOfEnemies = 3;
     public static int maxEnemies = 3;
+    public int newEnemyInterval = 3;
+
     float time;
     public int score;
     public GUIText scoreText;
@@ -23,27 +28,36 @@ public class GameScore : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
     {
-        time = Time.time;
+        currSpawnTime = spawnTime;
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
+        time = Time.time;
         score = currentScore;
         scoreText.text = "Score: " + score;
+        addMaxEnemies();
         addEnemy();
 
 	}
 
     void addEnemy()
-    {   
-        if (numberOfEnemies < maxEnemies)
+    {
+        if (numberOfEnemies < maxEnemies && ((int) time % newEnemyInterval == 0))
         {     
             Vector3 randCoor = new Vector3(Random.Range(xMin, xMax), originalLocation.y, Random.Range(zMin, zMax));
-
             Instantiate(target, randCoor, originalRotation);
-
             numberOfEnemies++;
         }
-    }   
+    }
+
+    void addMaxEnemies()
+    {
+        if ((int) time == currSpawnTime)
+        {
+            maxEnemies++;
+            currSpawnTime = currSpawnTime + spawnTime;
+        }
+    }
 }
